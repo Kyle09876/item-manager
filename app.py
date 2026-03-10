8e13aba854db29e912e3a6616f9e5b979a1be706
Kyle09876 <Kyle.shan@copeoplepr.com>
2026-03-10 17:20:59 +0800

    Update app.py

--- app.py
+++ app.py
@@ -533,5 +533,4 @@ def admin_save_order():
     return jsonify({'success': True, 'message': '顺序已保存'})
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=False)
+    app.run(host='0.0.0.0', port=5000, debug=True)
